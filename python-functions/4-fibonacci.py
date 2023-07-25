ba7698a1675bba9b730ff0a250deab9dcef5f0a2
oluwatobi <oluwatobiabass5@gmail.com>
2023-07-25 12:39:10 +0100

    Fibonacci sequence function

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def fibonacci_sequence(n):
+    result = []
     a, b = 0, 1
     while a < n:
-        print(a, end=', ')
+        result.append(a)
         a, b = b, a + b
-    print()
-    
+    return result
