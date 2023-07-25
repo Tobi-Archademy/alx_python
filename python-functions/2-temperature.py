deb9bcd4c5c5c8c9da1a03618d450649e28a0a60
oluwatobi <oluwatobiabass5@gmail.com>
2023-07-25 11:32:14 +0100

    updated task-2

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def convert_to_celsius(fahrenheit):
-    temp = float(input(fahrenheit))
-    cels = (temp - 32) * 5/9
+    try:
+        temp = float(input(fahrenheit))
+        cels = (temp - 32) * 5/9
+    except EOFError as e:
+        print(e)
     return cels
