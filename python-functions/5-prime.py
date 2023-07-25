ede612a6e3594955ac43eed6d03c12c0615f1cb8
oluwatobi <oluwatobiabass5@gmail.com>
2023-07-25 13:22:49 +0100

    updated task-5

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def is_prime(number):
-    for i in range(1, number):
+    for i in range(1, number + 1):
         if (number % i) == 0:
             return False
         return True
