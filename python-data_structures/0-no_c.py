ba0bc202ce4997b455c86c5dccc59874da5c2532
oluwatobi <oluwatobiabass5@gmail.com>
2023-07-26 23:21:52 +0100

    updated task-1

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -4,5 +4,5 @@ def no_c(my_string):
     new_string = ""
     for ch in my_string:
         if ch != "c" and ch != "C":
-            new_string = ch + new_string
+            new_string = ch
     return new_string
