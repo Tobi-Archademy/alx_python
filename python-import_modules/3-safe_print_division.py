be5e74b7b9bdb6a5ebad76546ffad89666b9cbce
oluwatobi <oluwatobiabass5@gmail.com>
2023-07-26 03:02:52 +0100

    updated task-3

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    result = a / b
     try:
-        result = a / b
-    except result == 0:
-        print("Inside result: None")
+        if result == 0:
+            print("Inside result: None")     
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
     finally:
         print("Inside result: {}".format(result))
