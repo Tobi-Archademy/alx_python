adcd92b61b7350d062c4d61ea608186521a672ba
oluwatobi <oluwatobiabass5@gmail.com>
2023-07-26 03:06:56 +0100

    Integers division with debug

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    result = a / b
     try:
-        if result == 0:
-            print("Inside result: None")     
-    except ValueError:
-        print("Oops!  That was no valid number.  Try again...")
+        result = a / b
+    except:
+        result = None
     finally:
         print("Inside result: {}".format(result))
+        return result
