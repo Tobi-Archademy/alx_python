988506a12841a3aeef90567f38241ee7385c6308
oluwatobi <oluwatobiabass5@gmail.com>
2023-08-09 10:05:56 +0100

    updated task-3

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -2,7 +2,17 @@
 """
 Contains the class BaseGeometry
 """
+class BaseMetaClass(type):
+    """
+    overrides.
+    """
 
+    def __dir__(cls):
+        return [
+            attribute
+            for attribute in super().__dir__()
+            if attribute != "__init_subclass__"
+        ]
 
 class BaseGeometry:
     """An empty class"""
