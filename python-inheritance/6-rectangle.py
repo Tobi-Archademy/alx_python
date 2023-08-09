508389ca5f4eb0c65e6be553f2027117b301cacd
oluwatobi <oluwatobiabass5@gmail.com>
2023-08-09 10:22:46 +0100

    updated task-6

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,11 +1,62 @@
 #!/usr/bin/python3
-"""
-contains the class BaseGeometry and subclass Rectangle
-"""
+""" An empty class representing the base geometry."""
+
+
+class BaseMetaClass(type):
+    """
+    overrides.
+    """
+
+    def __dir__(cls):
+        return [
+            attribute
+            for attribute in super().__dir__()
+            if attribute != "__init_subclass__"
+        ]
+
+
+class BaseGeometry(metaclass=BaseMetaClass):
+    """
+    Do nothing: By passing pass.
+    """
+
+    def __dir__(cls):
+        return [
+            attribute
+            for attribute in super().__dir__()
+            if attribute != "__init_subclass__"
+        ]
+
+    def area(self):
+        """
+        Public instance method that raises an Exception
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Public instance method that validate a value
+        Attr:
+            name(string): the name string.
+            value(int): must be an integer greater than 0.
+        """
+        self.name = name
+        self.value = value
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+"""Rectangle class that inherit from BaseGeometry"""
 
 
 class Rectangle(BaseGeometry):
-    """A representation of a rectangle"""
+
+    """Initializing with and height"""
+
     def __init__(self, width, height):
         """instantiation of the rectangle"""
         self.integer_validator("width", width)
