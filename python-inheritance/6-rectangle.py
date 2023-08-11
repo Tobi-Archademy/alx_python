f31f461013de0bba9f0b88e8096c3dc664268ec7
oluwatobi <oluwatobiabass5@gmail.com>
2023-08-11 12:16:43 +0100

    updated task-6

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -6,6 +6,18 @@ contains the class BaseGeometry and subclass Rectangle
 BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
 
+class BaseMetaClass(type):
+    """
+    overrides.
+    """
+
+    def __dir__(cls):
+        return [
+            attribute
+            for attribute in super().__dir__()
+            if attribute != "__init_subclass__"
+        ]
+
 class Rectangle(BaseGeometry):
 
     """Initializing with and height"""
