d327d2c5334958c20ea343889d3660051b3dceb1
oluwatobi <oluwatobiabass5@gmail.com>
2023-08-11 12:11:47 +0100

    updated task-6

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,55 +1,9 @@
-""" An empty class representing the base geometry."""
+#!/usr/bin/python3
+"""
+contains the class BaseGeometry and subclass Rectangle
+"""
 
-
-class BaseMetaClass(type):
-    """
-    overrides.
-    """
-
-    def __dir__(cls):
-        return [
-            attribute
-            for attribute in super().__dir__()
-            if attribute != "__init_subclass__"
-        ]
-
-
-class BaseGeometry(metaclass=BaseMetaClass):
-    """
-    Do nothing: By passing pass.
-    """
-
-    def __dir__(cls):
-        return [
-            attribute
-            for attribute in super().__dir__()
-            if attribute != "__init_subclass__"
-        ]
-
-    def area(self):
-        """
-        Public instance method that raises an Exception
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Public instance method that validate a value
-        Attr:
-            name(string): the name string.
-            value(int): must be an integer greater than 0.
-        """
-        self.name = name
-        self.value = value
-
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-"""Rectangle class that inherit from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
