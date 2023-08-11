4223c06ac396fafa75464c2b94fc8a3a0afc4f8b
oluwatobi <oluwatobiabass5@gmail.com>
2023-08-11 12:13:13 +0100

    updated task-6

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -3,7 +3,7 @@
 contains the class BaseGeometry and subclass Rectangle
 """
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
