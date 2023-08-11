04d1466159b4d08b14124d15a0ab9c1b3ac9368d
oluwatobi <oluwatobiabass5@gmail.com>
2023-08-11 08:19:01 +0100

    updated task-0, Base class

---
+++ python-almost_a_circle/README.md
@@ -0,0 +1,5 @@
+## Summary
+
+This file is the root directory for the python modules project folder. It contains all the relative tasks on the python-almost_a_circle project, which includes:
+
+* Base class
--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,3 +6,11 @@ Defines a class Base
 class Base:
     """Representation of a base"""
     __nb_objects = 0
+    
+    def __init__(self, id=None):
+        """Initializes the base"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
